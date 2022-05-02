a0d279e2e19cf9481811882eb101134b14f222b8
trumanfromkorea <jangjh971008@naver.com>
2022-05-02 19:40:27 +0900

    feat: 버튼 클릭 시 image, label 변경

--- SymbolRoller/SymbolRoller/SymbolRollerViewController.swift
+++ SymbolRoller/SymbolRoller/SymbolRollerViewController.swift
@@ -8,49 +8,46 @@
 import UIKit
 
 class SymbolRollerViewController: UIViewController {
-    
     // 이미지 system names
     let symbols: [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
 
     // interface builder 에 있는 UIComponent 와 코드를 연결
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var button: UIButton!
-    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var button: UIButton!
+
     // 화면이 보여지기 전에 준비를 하는 시간
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.image = UIImage(systemName: "cloud")
-        print("view did load")
-        // Do any additional setup after loading the view.
+
+        reload()
     }
-    
+
     // 화면이 보여질 준비가 되었을때
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("view will appear")
     }
-    
+
     // 화면이 보여지고 나서
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("view did appear")
     }
-    
+
+    // DRY ?
+    // Do not Repeat Yourself -> 중복된 코드 X
+    func reload() {
+        // TO-DO:
+        // - symbol 리스트에서 임의로 하나를 추출해서
+        // - 이미지와 텍스트를 설정한다
+
+        // return 값은 optional
+        let symbol = symbols.randomElement()!
+        imageView.image = UIImage(systemName: symbol)
+        label.text = symbol
+    }
+
     // 이벤트를 연결
     @IBAction func buttonTapped(_ sender: UIButton) {
-        print("button tapped!")
+        reload()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
