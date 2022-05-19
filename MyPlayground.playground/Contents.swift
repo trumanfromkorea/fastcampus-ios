8326bc83fc59a03bb8822c7834d0a2be8261e80e
trumanfromkorea <jangjh971008@naver.com>
2022-05-19 21:22:28 +0900

    create: collectionView 주식 리스트 프로젝트

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,8 +1,43 @@
 import UIKit
 
-var greeting = "Hello, playground"
+protocol HasArea {
+    var area: Double { get }
+}
 
-var num: Int = 30
+// HasArea 준수
+class Circle: HasArea {
+    let pi = 3.1415927
+    var radius: Double
+    var area: Double { return pi * radius * radius }
+    init(radius: Double) { self.radius = radius }
+}
 
+// HasArea 준수
+class Country: HasArea {
+    var area: Double
+    init(area: Double) { self.area = area }
+}
 
-var array: Array = Array<Int>()
+// HasArea 미준수
+class Animal {
+    var legs: Int
+    init(legs: Int) { self.legs = legs }
+}
+
+// 공유된 기본 클래스가 없지만 AnyObject 로 배열 생성
+let objects: [AnyObject] = [
+    Circle(radius: 2.0),
+    Country(area: 243_610),
+    Animal(legs: 4)
+]
+
+for object in objects {
+    if let objectWithArea = object as? HasArea {
+        print("Area is \(objectWithArea.area)")
+    } else {
+        print("Something that doesn't have an area")
+    }
+}
+// Area is 12.5663708
+// Area is 243610.0
+// Something that doesn't have an area
---
+++ StockRank/StockRank/StockRankViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StockRankViewController.swift
+//  StockRank
+//
+//  Created by 장재훈 on 2022/05/19.
+//
+
+import UIKit
+
+class StockRankViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
