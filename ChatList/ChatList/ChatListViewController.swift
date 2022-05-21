f779649396103c7e7eaf3d7e504116e49e67edf7
trumanfromkorea <jangjh971008@naver.com>
2022-05-21 21:38:40 +0900

    style: autolayout 적용

---
+++ ChatList/ChatList/ChatListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatListViewController.swift
+//  ChatList
+//
+//  Created by 장재훈 on 2022/05/21.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
