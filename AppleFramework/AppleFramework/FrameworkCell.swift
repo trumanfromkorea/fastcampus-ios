9f46418ff22b4d60945cafd045a4e2a59481532b
trumanfromkorea <jangjh971008@naver.com>
2022-05-23 22:47:50 +0900

    feat: collectionView grid 부분 마무리

--- AppleFramework/AppleFramework/FrameworkCell.swift
+++ AppleFramework/AppleFramework/FrameworkCell.swift
@@ -13,6 +13,13 @@ class FrameworkCell: UICollectionViewCell {
     @IBOutlet var thumbnailImageView: UIImageView!
     @IBOutlet var nameLabel: UILabel!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        nameLabel.numberOfLines = 1
+        nameLabel.adjustsFontSizeToFitWidth = true // 알아서 폰트 사이즈 조정
+    }
+
     func configure(_ framework: AppleFramework) {
         thumbnailImageView.image = UIImage(named: framework.imageName)
         nameLabel.text = framework.name
--- AppleFramework/AppleFramework/FrameworkListViewController.swift
+++ AppleFramework/AppleFramework/FrameworkListViewController.swift
@@ -11,6 +11,7 @@ class FrameworkListViewController: UIViewController {
     @IBOutlet var collectionView: UICollectionView!
 
     let list: [AppleFramework] = AppleFramework.list
+    let HInset: CGFloat = 15
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,22 +19,42 @@ class FrameworkListViewController: UIViewController {
         // data, presentation, layout
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        navigationController?.navigationBar.topItem?.title = "🍏 Apple Frameworks"
+        
+        // estimated size 를 코드로
+        if let flowlayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowlayout.estimatedItemSize = .zero
+        }
+        
+        // 상하좌우 insets
+        collectionView.contentInset = UIEdgeInsets(top: 10, left: HInset, bottom: 0, right: HInset)
+    }
+}
+
+extension FrameworkListViewController: UICollectionViewDelegate {
+    // 아이템 선택 시 호출
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let framework = list[indexPath.item]
+        
+        print(framework.name)
     }
 }
 
 extension FrameworkListViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let interItemSpacing: CGFloat = 10
-        let width = (collectionView.bounds.width - (interItemSpacing * 2)) / 3
+        let width = (collectionView.bounds.width - (interItemSpacing * 2 + HInset * 2)) / 3
         let height = width * 1.5
 
         return CGSize(width: width, height: height)
     }
-    
+
     // 좌우 간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 10
     }
+
     // 상하 간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 10
