be60fdc620585a6ad2b65247ade806dc2eca89b8
trumanfromkorea <jangjh971008@naver.com>
2022-05-24 20:40:22 +0900

    feat: collectionview 설정

--- InstaSearchView/InstaSearchView/SearchViewController.swift
+++ InstaSearchView/InstaSearchView/SearchViewController.swift
@@ -9,26 +9,54 @@ import UIKit
 
 class SearchViewController: UIViewController {
     @IBOutlet var collectionView: UICollectionView!
+
+    // data, presentation, layout
     override func viewDidLoad() {
         super.viewDidLoad()
 
         collectionView.delegate = self
         collectionView.dataSource = self
+
+        // cell 크기 지정하는대로 출력하기 위함
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = .zero
+        }
+        
+        
     }
 }
 
 extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let interItemSpacing: CGFloat = 1
+        let width = (collectionView.bounds.width - interItemSpacing * 2 ) / 3
+        let height = width
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
 }
 
 extension SearchViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return 24
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ResultCell.identifier, for: indexPath) as? ResultCell else {
             return UICollectionViewCell()
         }
+
+        let imageName = "animal\(indexPath.item + 1)"
+        cell.configure(imageName)
 
         return cell
     }
