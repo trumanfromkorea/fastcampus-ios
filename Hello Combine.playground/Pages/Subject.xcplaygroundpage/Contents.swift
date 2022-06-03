0529460e3680b1fc1556d6d6a6e1f1c998a68f1d
trumanfromkorea <jangjh971008@naver.com>
2022-06-03 12:39:42 +0900

    study: Combine

--- Hello Combine.playground/Pages/Subject.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Subject.xcplaygroundpage/Contents.swift
@@ -1,14 +1,36 @@
-import Foundation
 import Combine
+import Foundation
 
-// PassthroughSubject
+// MARK: -PassthroughSubject
 
+// output type String
+// failure type Never - 실패할리 없다
+let relay = PassthroughSubject<String, Never>()
+let subscription1 = relay.sink { value in
+    print("subscription1 received value: \(value)")
+}
 
+relay.send("Hello")
+relay.send("World!")
 
+// MARK: - CurrentValueSubject
+// output type 에 대한 초기값이 필요함
 
-// CurrentValueSubject
+let variable = CurrentValueSubject<String, Never>("")
+variable.send("Initial text")
 
+let subscription2 = variable.sink { value in
+    print("subscription2 received value: \(value)")
+}
 
+variable.send("More text")
+// --> subscription2 received value:
+// --> subscription2 received value: More text
+//
+// subscription 이 생겼을때 기존에 가지고 있던 value 한번 보내고
+// 그 다음에 받은 value 보냄
+variable.value
 
-
-
+let publisher = ["Here", "we", "go"].publisher
+publisher.subscribe(relay)
+publisher.subscribe(variable)
--- Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -1,12 +1,24 @@
 //: [Previous](@previous)
 
-import Foundation
 import Combine
+import Foundation
 
-
-
+let subject = PassthroughSubject<String, Never>()
 
 // The print() operator prints you all lifecycle events
+let subscription = subject
+    .print("[Debug]")
+    .sink { value in
+        print("Subscriber received value: \(value)")
+    }
 
+subject.send("Hello")
+subject.send("Hello again!")
+subject.send("Hello for the last time!")
+
+//subject.send(completion: .finished) // --> publisher 에서 끊는 경우
+subscription.cancel() // --> subscriber 에서 끊는 경우
+
+//subject.send("Hello ?? :(")
 
 //: [Next](@next)
