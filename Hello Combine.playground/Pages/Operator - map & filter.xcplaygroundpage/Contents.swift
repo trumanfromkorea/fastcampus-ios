8ae9e38cb95dc062311380c8f38dfa206f054b55
trumanfromkorea <jangjh971008@naver.com>
2022-06-04 09:58:28 +0900

    study: combine scheduler & operator

--- Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
@@ -1,12 +1,34 @@
 //: [Previous](@previous)
 
-import Foundation
 import Combine
+import Foundation
 
 // Transform - Map
+let numPublisher = PassthroughSubject<Int, Never>()
+let subscription1 = numPublisher
+    .map { $0 * 2 }
+    .sink { value in
+        print("Transformed Value: \(value)")
+    }
 
+numPublisher.send(10)
+numPublisher.send(20)
+numPublisher.send(30)
+subscription1.cancel()
 
 // Filter
+let stringPublisher = PassthroughSubject<String, Never>()
+let subscription2 = stringPublisher
+    .filter { $0.contains("a") }
+    .sink { value in
+        print("Filtered Value: \(value)")
+    }
 
+stringPublisher.send("abc")
+stringPublisher.send("zoom")
+stringPublisher.send("joon")
+stringPublisher.send("jennie")
+stringPublisher.send("jason")
+subscription2.cancel()
 
 //: [Next](@next)
--- Hello Combine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
@@ -1,10 +1,25 @@
 //: [Previous](@previous)
 
-import Foundation
 import Combine
+import Foundation
 
+let arrayPublisher = [1, 2, 3].publisher
 
+let queue = DispatchQueue(label: "custom")
 
-
+/*
+ custom thread 에서 작업을 수행한 후
+ main thread 에서 데이터를 받는 과정
+ */
+let subscription = arrayPublisher
+    .subscribe(on: queue)
+    .map { value -> Int in
+        print("transform: \(value), thread: \(Thread.current)")
+        return value
+    }
+    .receive(on: DispatchQueue.main)
+    .sink { value in
+        print("Received Value: \(value), thread: \(Thread.current)")
+    }
 
 //: [Next](@next)
