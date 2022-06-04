5ec4296903df8c0f06cb7a4f6c71f55b696ef27a
trumanfromkorea <jangjh971008@naver.com>
2022-06-04 11:21:09 +0900

    study: Combine 실습 완료

--- Hello Combine.playground/Pages/Operator - combineLatest.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Operator - combineLatest.xcplaygroundpage/Contents.swift
@@ -1,22 +1,58 @@
 //: [Previous](@previous)
 
-import Foundation
 import Combine
-
+import Foundation
 
 // Basic CombineLatest
 
+let strPublisher = PassthroughSubject<String, Never>()
+let numPublisher = PassthroughSubject<Int, Never>()
 
+Publishers.CombineLatest(strPublisher, numPublisher)
+    .sink { str, num in
+        print("Receive: \(str), \(num)")
+    }
 
+// strPublisher.combineLatest(numPublisher).sink { str, num in
+//    print("Receive: \(str), \(num)")
+// }
 
+strPublisher.send("a")
+numPublisher.send(1)
+strPublisher.send("b")
+strPublisher.send("c")
+numPublisher.send(2)
+numPublisher.send(3)
 
 // Advanced CombineLatest
+let usernamePublisher = PassthroughSubject<String, Never>()
+let passwordPublisher = PassthroughSubject<String, Never>()
 
+let validatedCrendentialsSubscription = usernamePublisher.combineLatest(passwordPublisher)
+    .map { username, password -> Bool in
+        !username.isEmpty && !password.isEmpty && password.count > 12
+    }
+    .sink { valid in
+        print("Credential valid? : \(valid)")
+    }
 
-
+usernamePublisher.send("jasonLee")
+passwordPublisher.send("weakpw")
+passwordPublisher.send("strongPassword")
 
 // Merge
 
+let publisher1 = [1, 2, 3, 4, 5].publisher
+let publisher2 = [300, 400, 500].publisher
 
+Publishers.Merge(publisher1, publisher2)
+    .sink { value in
+        print("Merge: subscription received value: \(value)")
+    }
+
+//let mergePublisherSubscription = publisher1.merge(with: publisher2)
+//    .sink { value in
+//        print("Merge: subscription received value: \(value)")
+//    }
 
 //: [Next](@next)
--- Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
@@ -1,23 +1,48 @@
 //: [Previous](@previous)
 
-import Foundation
 import Combine
+import Foundation
 
 var subscriptions = Set<AnyCancellable>()
 
 // removeDuplicates
+let words = "hey hey there! Mr Mr ?"
+    .components(separatedBy: " ")
+    .publisher
 
+words
+    .removeDuplicates()
+    .sink { value in
+        print(value)
+    }
+    .store(in: &subscriptions)
 
 // compactMap
+let strings = ["a", "1.24", "3", "def", "45"].publisher
 
-
+strings
+    .compactMap { Float($0) }
+    .sink { value in
+        print(value)
+    }
+    .store(in: &subscriptions)
 
 // ignoreOutput
 
+let numbers = (1 ... 10000).publisher
 
+numbers
+    .ignoreOutput()
+    .sink(receiveCompletion: { print("Completed with: \($0)") }, receiveValue: { print($0) })
+    .store(in: &subscriptions)
 
 // prefix
 
+let tens = (1 ... 10).publisher
 
+tens
+    .prefix(2)
+    .sink(receiveCompletion: { print("Completed with: \($0)") }, receiveValue: { print($0) })
+    .store(in: &subscriptions)
 
 //: [Next](@next)
