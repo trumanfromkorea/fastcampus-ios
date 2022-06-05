9e7d33322bb5077979f12e9f79970dd95ffb23f1
trumanfromkorea <jangjh971008@naver.com>
2022-06-05 19:18:06 +0900

    refactor: apple framework combine 전환

--- AppleFramework_Combine/AppleFramework/FrameworkListViewController.swift
+++ AppleFramework_Combine/AppleFramework/FrameworkListViewController.swift
@@ -5,25 +5,69 @@
 //  Created by joonwon lee on 2022/04/24.
 //
 
+import Combine
 import UIKit
 
 class FrameworkListViewController: UIViewController {
-    
-    @IBOutlet weak var collectionView: UICollectionView!
-    
+    @IBOutlet var collectionView: UICollectionView!
+
     typealias Item = AppleFramework
     enum Section {
         case main
     }
-    
+
     var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
-    
-    let list: [AppleFramework] = AppleFramework.list
-    
+
+    // Combine
+    var subscriptions = Set<AnyCancellable>()
+    let didSelect = PassthroughSubject<AppleFramework, Never>()
+    let items = CurrentValueSubject<[AppleFramework], Never>(AppleFramework.list)
+//    @Published var list: [AppleFramework] = AppleFramework.list
+
     // Data, Presentation, Layout
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // CollectionView Presentation, Layout 설정
+        // CollectionView Data 설정
+
+        configureCollectionView()
+        bind()
+    }
+
+    private func bind() {
+        // input : 사용자 인풋을 받아서, 처리해야할 것
+        // - item 선택 되었을때 처리
+        didSelect
+            .receive(on: RunLoop.main) // UI 니까 main 에서
+            .sink { [unowned self] framework in
+                let sb = UIStoryboard(name: "Detail", bundle: nil)
+                let vc = sb.instantiateViewController(withIdentifier: "FrameworkDetailViewController") as! FrameworkDetailViewController
+                vc.framework = framework
+
+                self.present(vc, animated: true)
+            }
+            .store(in: &subscriptions)
+
+        // output : data, state 변경에 따라서, UI 업데이트 할것
+        // - items 세팅이 되었을때 view 업데이트
+        items
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] list in
+                self.applySectionItems(list)
+            }
+            .store(in: &subscriptions)
+    }
+
+    private func applySectionItems(_ items: [Item], to section: Section = .main) {
+        // data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([section])
+        snapshot.appendItems(items, toSection: section)
+        dataSource.apply(snapshot)
+    }
+
+    private func configureCollectionView() {
         // presentation
         dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FrameworkCell", for: indexPath) as? FrameworkCell else {
@@ -32,48 +76,39 @@ class FrameworkListViewController: UIViewController {
             cell.configure(item)
             return cell
         })
-        
-        // data
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(list, toSection: .main)
-        dataSource.apply(snapshot)
-        
+
         // layer
         collectionView.collectionViewLayout = layout()
-        
+
         collectionView.delegate = self
     }
-    
+
     private func layout() -> UICollectionViewCompositionalLayout {
         let spacing: CGFloat = 10
         // Item
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.33), heightDimension: .fractionalWidth(0.33))
         let itemLayout = NSCollectionLayoutItem(layoutSize: itemSize)
-        
+
         // Group
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.33))
-        let groupLayout = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: itemLayout, count:   3)
+        let groupLayout = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: itemLayout, count: 3)
         groupLayout.interItemSpacing = .fixed(spacing)
-        
+
         // Section
         let section = NSCollectionLayoutSection(group: groupLayout)
         section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
         section.interGroupSpacing = spacing
-        
+
         return UICollectionViewCompositionalLayout(section: section)
     }
 }
 
 extension FrameworkListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let framework = list[indexPath.item]
+//        let framework = list[indexPath.item]
+        let framework = items.value[indexPath.item]
         print(">>> selected: \(framework.name)")
-        
-        let sb = UIStoryboard(name: "Detail", bundle: nil)
-        let vc = sb.instantiateViewController(withIdentifier: "FrameworkDetailViewController") as! FrameworkDetailViewController
-        vc.framework = framework
-        
-        present(vc, animated: true)
+
+        didSelect.send(framework)
     }
 }
